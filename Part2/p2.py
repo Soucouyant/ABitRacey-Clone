c26b4b4c0a71dc763e330767cdaac26c6c0703a0
Soucouyant <duranr@protonmail.com>
2022-04-14 11:17:19 -0400

    finished Part 2

--- Part2/p2.py
+++ Part2/p2.py
@@ -8,13 +8,22 @@ screen = pygame.display.set_mode((constants.Width, constants.Height))
 pygame.display.set_caption(constants.gameName)
 clock = pygame.time.Clock()
 
+carImg  = pygame.image.load('./Part2/assets/bigGreenCar.png')
+
+def car(x,y):
+    screen.blit(carImg, (x, y))
+    
+x = (constants.Width * 0.4)
+y = (constants.Height * 0.6)
+
 while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
-        screen.fill(constants.black)
+        screen.fill(constants.white)
+        car(x,y)
         
         pygame.display.update()
         clock.tick(60)
