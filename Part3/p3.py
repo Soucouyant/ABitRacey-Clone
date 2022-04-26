29add25745be1fe8d569e2024348654d88b687df
Soucouyant <duranr@protonmail.com>
2022-04-26 09:48:48 -0400

    finsihed Part 3 - Movement works

--- Part3/p3.py
+++ Part3/p3.py
@@ -13,29 +13,33 @@ carImg  = pygame.image.load('./Part2/assets/bigGreenCar.png')
 def car(x,y):
     screen.blit(carImg, (x, y))
     
-x = (constants.Width * 0.4)
-y = (constants.Height * 0.6)
+x = (constants.Width * 0.45)
+y = (constants.Height * 0.8)
+
 xChange = 0
 yChange = 0
+carSpeed = 0
 
 while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
-            # Movement Handler
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    xChange = -5
-                if event.key == pygame.K_RIGHT:
-                    xChange = 5
-                if event.key == pygame.K_UP:
-                    yChange = 5
-                if event.key == pygame.K_DOWN:
-                    yChange = -5
-            x = x + xChange
-            y = y + yChange
+        # Movement Handler
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                xChange = -5
+            if event.key == pygame.K_RIGHT:
+                xChange = 5
+            if event.key == pygame.K_UP:
+                yChange = -5
+            if event.key == pygame.K_DOWN:
+                yChange = 5
+        x = x + xChange
+        xChange = 0
+        y = y + yChange
+        yChange = 0
                     
         screen.fill(constants.white)
         car(x,y)
