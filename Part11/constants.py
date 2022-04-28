cd671006cc49d09827af8213b53d16262687472f
Soucouyant <duranr@protonmail.com>
2022-04-28 14:44:58 -0400

    started Part 11 - Buttons don't display

---
+++ Part11/constants.py
@@ -0,0 +1,7 @@
+Width = 800
+Height = 400
+gameName = 'A Bit Racey'
+black = (0,0,0)
+white = (255,255,255)
+red = (209,25,25)
+green = (50,191,87)
---
+++ Part11/p11.py
@@ -0,0 +1,134 @@
+import pygame
+import constants
+import time
+import random
+from sys import exit
+
+# Initiliaze
+pygame.init()
+screen = pygame.display.set_mode((constants.Width, constants.Height))
+pygame.display.set_caption(constants.gameName)
+clock = pygame.time.Clock()
+
+carImg  = pygame.image.load('./Part11/assets/bigGreenCar.png')
+roboto = pygame.font.Font('./Part11/assets/Roboto-Black.ttf', 27)
+
+def blocksDodged(count):
+    text = roboto.render("Dodged: "+str(count), True, constants.black)
+    screen.blit(text, (0,0)) 
+
+def car(x,y):
+    screen.blit(carImg, (x, y))
+    
+def textObj(text, font):
+    textSurface = font.render(text, True, constants.black)
+    return textSurface, textSurface.get_rect()
+
+def textDisplay(text):
+    TextSurf, TextRect = textObj(text, roboto)
+    TextRect.center = ((constants.Width/2),(constants.Height/2))
+    screen.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+    
+    time.sleep(2)
+    
+    gameLoop()
+    
+def crash():
+    textDisplay('You Crashed!')
+    
+def randomColor():
+    r = random.randrange(0,255)
+    g = random.randrange(0,255)
+    b = random.randrange(0,255)
+    return r,g,b
+    
+def blockGen(blockX, blockY, blockW, blockH, color):
+    pygame.draw.rect(screen, color, [blockX, blockY, blockW, blockH])
+    
+def splashScreen():
+    intro = True
+    
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        screen.fill(constants.white)
+        TextSurf, TextRect = textObj("A bit Racey", roboto)
+        TextRect.center = ((constants.Width/2), (constants.Height/2))
+        screen.blit(TextSurf, TextRect)
+        
+        # Buttons
+        pygame.draw.rect(screen, constants.red, (150,450,100,50)) # Red
+        pygame.draw.rect(screen, constants.green, (550,450,100,50)) # Green
+        
+        pygame.display.update()
+        clock.tick(15)
+    
+def gameLoop():
+    x = (constants.Width * 0.45)
+    y = (constants.Height * 0.627)
+
+    xChange = 0
+    carWidth = 150 - 38
+    
+    blockStartX = random.randrange(0, constants.Width)
+    blockStartY = -600
+    blockSpeed = 7
+    blockWidth = 100
+    blockHeight = 100
+    
+    blockCount = 1
+    dodged = 0
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+            # Movement Handler
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    xChange = -5
+                if event.key == pygame.K_RIGHT:
+                    xChange = 5
+                    
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                    xChange = 0
+                    
+        x = x + xChange
+        screen.fill(constants.white)
+        
+        blockGen(blockStartX, blockStartY, blockWidth, blockHeight, constants.black)
+        blockStartY += blockSpeed
+        car(x,y)
+        
+        blockStartY += blockSpeed
+        blocksDodged(dodged)
+        
+        if x > constants.Width - carWidth or x < 0:
+            crash()
+            
+        if blockStartY > constants.Height:
+            blockStartY = 0 - blockHeight
+            blockStartX = random.randrange(0,constants.Width)
+            dodged += 1
+            blockSpeed += .15
+            blockWidth += (dodged * 1.2)
+        
+        if y < blockStartY+blockHeight:  
+            if x > blockStartX and x < blockStartX+blockWidth or x+carWidth > blockStartX and x + carWidth < blockStartX + blockWidth:
+                crash()
+            
+       
+        pygame.display.update()
+        clock.tick(60)
+        
+# Run Game
+splashScreen()
+gameLoop()
