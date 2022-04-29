84d06455a2d84cd16d11bf4fc0ab9af360896b45
Soucouyant <duranr@protonmail.com>
2022-04-29 11:20:09 -0400

    fixed unused import statements

---
+++ Part14/constants.py
@@ -0,0 +1,15 @@
+# Game Info
+Width = 800
+Height = 600
+gameName = 'A Bit Racey'
+
+# Base Colors
+black = (0,0,0)
+white = (255,255,255)
+
+# OBJ Colors
+red = (201,12,12)
+lightRed = (255,46,46)
+
+lightGreen = (47,222,70)
+green = (2,181,26)
